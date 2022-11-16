f1a862d92891bcea2f8d4b7b064d5d686fc676ef
Le Trong Dat <datlt2306@gmail.com>
2022-11-16 11:04:27 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,23 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Product from "./components/product";
 import ProductEdit from "./components/product-edit";
+import LayoutAdmin from "./layouts/LayoutAdmin";
+import LayoutWebsite from "./layouts/LayoutWebsite";
 
 function App() {
     return (
         <div className="App">
             <Routes>
-                <Route path="/" element={<h1>Home Page</h1>} />
-                <Route path="products" element={<Product />} />
-                <Route path="products/add" element={<h1>Product Add</h1>} />
-                <Route path="products/:id/edit" element={<ProductEdit />} />
+                <Route path="/" element={<LayoutWebsite />}>
+                    <Route index element={<h1>Home Page</h1>} />
+                    <Route path="about" element={<h1>About Page</h1>} />
+                </Route>
+                <Route path="/admin" element={<LayoutAdmin />}>
+                    <Route index element={<h1>Dashboard</h1>} />
+                    <Route path="products" element={<Product />} />
+                    <Route path="products/add" element={<h1>Product Add</h1>} />
+                    <Route path="products/:id/edit" element={<ProductEdit />} />
+                </Route>
             </Routes>
         </div>
     );
