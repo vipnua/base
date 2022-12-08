6bae175aec3226b2894eee06c197cee9bcbbb12a
Le Trong Dat <datlt2306@gmail.com>
2022-12-08 22:12:31 +0700

    remove router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
-import { useState } from "react";
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import LayoutAdmin from "./layouts/LayoutAdmin";
-import LayoutWebsite from "./layouts/LayoutWebsite";
 
 function App() {
-    return (
-        <div className="App">
-            <Routes>
-                <Route path="/" element={<LayoutWebsite />}>
-                    <Route index element={<h1>Home Page</h1>} />
-                    <Route path="about" element={<h1>About Page</h1>} />
-                </Route>
-                <Route path="/admin" element={<LayoutAdmin />}>
-                    <Route index element={<h1>Dashboard</h1>} />
-                    <Route path="products" element={<h1>Product Lis</h1>} />
-                    <Route path="products/add" element={<h1>Product Add</h1>} />
-                    <Route path="products/:id/edit" element={<h1>Product Edit</h1>} />
-                </Route>
-            </Routes>
-        </div>
-    );
+    return <div className="App"></div>;
 }
 
 export default App;
